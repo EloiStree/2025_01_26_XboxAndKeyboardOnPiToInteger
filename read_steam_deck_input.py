aa5c082b52bc01a2cad5ca935a0109ded2bbe271
Éloi Strée <eloistree@gmail.com>
2025-04-18 19:20:11 +0200

    Train TrainNote

---
+++ read_steam_deck_input.py
@@ -0,0 +1,9 @@
+=hile True:
+        gamepads.clear()
+        # List all input devices
+        devices = [InputDevice(path) for path in list_devices()]
+
+
+
+
+        for device in devices:                                      */-+*
